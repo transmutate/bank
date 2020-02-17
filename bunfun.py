3bb57f9f5511c7e5d3fe36b0e1248c3d261f7fa0
transmutate <47515367+transmutate@users.noreply.github.com>
2020-02-17 13:38:25 +0900

    updated_to_display_as_df

--- bunfun.py
+++ bunfun.py
@@ -17,72 +17,37 @@ def isid(ds, var, only_based_on_non_miss=True):
 # like the good old ones from stata back in the day
 def tab(var, incl_miss=True, sort_by='alp'):
     """ STATA'S TAB COMMAND """
+    var = var.astype(str)
     tot = len(var)
     dict_c = {}
 
-    if var.dtype == object:
-        miss_count = 0
-        for i in var:
-            if len(i) > 0:
-                if i in dict_c:
-                    dict_c[i] += 1
-                else:
-                    dict_c[i] = 1
+    miss_count = 0
+    for i in var:
+        if i != 'nan':
+            if i in dict_c:
+                dict_c[i] += 1
             else:
-                miss_count += 1
-    else:
-        miss_count = 0
-        for i in var:
-            if ~np.isnan(i):
-                if i in dict_c:
-                    dict_c[i] += 1
-                else:
-                    dict_c[i] = 1
-            else:
-                miss_count += 1
+                dict_c[i] = 1
+        else:
+            miss_count += 1
 
     if sort_by == 'alp':
-        a = sorted(list(dict_c.keys()))
-        if miss_count > 0:
-            a.append('MISSING')
-            dict_c['MISSING'] = miss_count
-    elif sort_by == 'asc':
-#     else:
-        if miss_count > 0:
-            dict_c['MISSING'] = miss_count
-        b = sorted(dict_c.items(),key=lambda x: x[1])
-        a = [i[0] for i in b]
+        t = pd.DataFrame.from_dict(dict_c, orient='index', columns=['count']).sort_index()
+        t = t.append(pd.DataFrame({'count':miss_count}, index=['MISSING']))
+    elif sort_by == 'des':
+        t = pd.DataFrame.from_dict(dict_c, orient='index', columns=['count'])
+        t = t.append(pd.DataFrame({'count':miss_count}, index=['MISSING']))
+        t.sort_values('count',ascending=False, inplace=True)
     else:
-        if miss_count > 0:
-            dict_c['MISSING'] = miss_count
-        b = sorted(dict_c.items(),key=lambda x: x[1], reverse=True)
-        a = [i[0] for i in b]
+        t = pd.DataFrame.from_dict(dict_c, orient='index', columns=['count'])
+        t = t.append(pd.DataFrame({'count':miss_count}, index=['MISSING']))
+        t.sort_values('count', inplace=True)
 
-    a.append('TOTAL')
-    dict_c['TOTAL'] = tot
+    t['perc'] = t['count'] / t['count'].sum()
+    t['cum_perc'] = t['perc'].cumsum()
 
-    mll = 0                  # as in Maximum Lenth for Label
-    mlv = len("Freq")        # as in Maximum Lenth for Value
-    mlp = len("Perc")        # as in Maximum Lenth for Percentage
-    for key, value in dict_c.items():
-        dict_c[key] = ["{:,}".format(value), "{:.2%}".format(value / tot)]
+    t = t.append(pd.DataFrame({'count':[tot], 'perc':[1]}, index=['TOTAL']))
 
-        mll = max(mll, len(str(key)))
-        mlv = max(mlv, len(dict_c[key][0]))
-        mlp = max(mlp, len(dict_c[key][1]))
-
-    print(" "*mll + "   " + "Freq"+ " "*(mlv-4) + "   " + "Perc"+ " "*(mlp-4) + "   Cum. Perc")
-    cp = 0
-    for i in a:
-        for key, val in dict_c.items():
-            if i == key:
-                if i == 'TOTAL':
-                    cp_out = ''
-                else:
-                    cp = cp + float(val[0].replace(',',''))
-                    cp_out = "{:.2%}".format(cp / tot)
-
-                print(str(key) + " "*(mll-len(str(key))) + "   " + str(val[0]) + " "*(mlv-len(str(val[0]))) + "   " + str(val[1]) + \
-                      " "*(mlp-len(str(val[1]))) + "   " + cp_out  )
-            else:
-                continue
+    t = t[['count','perc','cum_perc']]
+    T = t.style.format({'count':'{:,}', 'perc':'{:.2%}', 'cum_perc':'{:.2%}'})
+    display(T)
